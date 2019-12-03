c84858824f6f73f8e92167c9c852901b0a03054c
FelixSolano <52423830+FelixSolano@users.noreply.github.com>
2019-12-02 22:13:09 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,34 @@
+
+
+$(document).ready(function () {
+
+	mostrarinicio();
+
+});
+
+
+
+function mostrarinicio(){
+	
+	$.get("inicio.html", function (response) {
+
+		$("#divcontenido").html(response);
+	
+	});
+
+	
+}
+
+
+
+function grabado(){
+	
+	
+alert("SR(@)"+$("#txtnombres").val() + " fue grabado "+ $("#txtcelulares").val());
+	
+
+}
+
+
+
+
